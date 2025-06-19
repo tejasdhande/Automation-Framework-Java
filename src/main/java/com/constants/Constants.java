8f17370fe0dad86d5a6c62175064f5618d97a444
Tejas <tejas.dhande@kanakasoftware.com>
2025-06-19 18:10:38 +0530

    Added Test data properties file

--- src/main/java/com/constants/Constants.java
+++ src/main/java/com/constants/Constants.java
@@ -5,4 +5,8 @@ public abstract class Constants {
 	public static final String CURRENT_DIR = System.getProperty("user.dir");  //return path of current working directory
 	
 	public static final String EXECUTATION_REPORT_PATH = CURRENT_DIR+"/reports/execution.html";
+	
+	public static final int EXPLICIT_WAIT_TIME = 20;
+	
+	public static final String CONFIG_FILE_PATH = CURRENT_DIR+"/src/test/resources/execution/config.properties";
 }
--- src/main/java/com/iframe/IframeActions.java
+++ src/main/java/com/iframe/IframeActions.java
@@ -12,52 +12,52 @@ import com.driver.DriverFactory;
 import com.report.ExtentFactory;
 
 public class IframeActions {
-	
+
 	private Logger logger = LogManager.getLogger(IframeActions.class);
-	
+
 	public void waitForIframeAndSwitch(String idOrName) {
-		
+
 		try {
-			WebDriverWait wait = new WebDriverWait(DriverFactory.getInstace().getDriver(), Duration.ofSeconds(Constants.EXPLICIT_WAIT_TIME));
-			
+			WebDriverWait wait = new WebDriverWait(DriverFactory.getInstace().getDriver(),
+					Duration.ofSeconds(Constants.EXPLICIT_WAIT_TIME));
+
 			wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(idOrName));
 			logger.info("Switched to iframe");
 			ExtentFactory.getInstance().passTest("Switched to iframe");
-			
+
 		} catch (Exception e) {
 			logger.error("Error occured while switch to Iframe");
 			ExtentFactory.getInstance().failTest("Error occurred while switching to frame");
-			
-			
+
 		}
-			
+
 	}
 
 	public void waitForIframeAndSwitch(int index) {
-		
+
 		try {
-			WebDriverWait wait = new WebDriverWait(DriverFactory.getInstace().getDriver(), Duration.ofSeconds(Constants.EXPLICIT_WAIT_TIME));
-			
+			WebDriverWait wait = new WebDriverWait(DriverFactory.getInstace().getDriver(),
+					Duration.ofSeconds(Constants.EXPLICIT_WAIT_TIME));
+
 			wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(index));
 			logger.info("Switched to iframe");
 			ExtentFactory.getInstance().passTest("Switched to iframe");
-			
+
 		} catch (Exception e) {
 			logger.error("Error occured while switch to Iframe");
 			ExtentFactory.getInstance().failTest("Error occurred while switching to frame");
-			
-			
+
 		}
-			
+
 	}
-	
+
 	public void switchBackToDefaultContent() {
-		 
+
 		try {
 			DriverFactory.getInstace().getDriver().switchTo().defaultContent();
 			logger.info("Switched to Default Content");
 			ExtentFactory.getInstance().passTest("Switched to Default Content");
-			
+
 		} catch (Exception e) {
 			logger.error("Error occured while switching to Default content");
 			ExtentFactory.getInstance().failTest("Error occurred while switching to default content ");
